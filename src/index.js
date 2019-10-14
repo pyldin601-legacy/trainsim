fce47b8112a49513cdcf7f30064d019577c4b0ed
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-10-15 00:42:30 +0300

    Simulate wheels (WIP)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import {
   accelerate$,
   brake$,
+  calculateWheelsOffsets,
   createDistance$,
   createPath$,
   createSpeed$,
   generateRandomPath
 } from "./domain/engine";
-import { share } from "rxjs/operators";
+import { mergeAll, share, map, mergeMap } from "rxjs/operators";
+import { of } from "rxjs";
 
 const accelerateElement = document.getElementById("accelerate");
 const brakeElement = document.getElementById("brake");
@@ -22,7 +24,6 @@ brakeElement.addEventListener("mouseup", () => brake$.next(false));
 
 const speed$ = createSpeed$(accelerate$, brake$).pipe(share());
 const distance$ = createDistance$(speed$).pipe(share());
-const road$ = createPath$(generateRandomPath(), distance$);
 
 speed$.subscribe(spd => {
   speedElement.innerText = Number(spd).toFixed(1);
@@ -32,4 +33,16 @@ distance$.subscribe(dst => {
   distanceElement.innerText = Number(dst).toFixed(3);
 });
 
-road$.subscribe(console.log);
+const wheels = calculateWheelsOffsets(0);
+console.log(wheels);
+
+const all$ = of(
+  wheels.map(d =>
+    createPath$(generateRandomPath(), distance$.pipe(map(dd => dd + d / 1000)))
+  )
+).pipe(
+  mergeMap(it => it),
+  mergeAll()
+);
+
+all$.subscribe(console.log);
